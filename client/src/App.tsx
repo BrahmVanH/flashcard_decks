6dca54102053840ffc1d42d7be19f88a6227d627
Brahm V <brahmvanh@gmail.com>
2023-10-27 17:34:11 -0400

    added apollo support to client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache,
+	createHttpLink,
+} from '@apollo/client';
+
 import './App.css';
 
 import Home from './pages/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const httpLink = createHttpLink({
+	uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+
+	cache: new InMemoryCache(),
+});
 
 function App() {
 	return (
