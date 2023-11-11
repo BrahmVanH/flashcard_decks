2cd4da8c69f0a021715ff463dfbb68e5aa3b1b8c
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-10 20:34:07 -0500

    added more typing to deck controllers. Going to hold off on the update function, as I think im going to change the way it works

--- server/controllers/deckController.ts
+++ server/controllers/deckController.ts
@@ -1,6 +1,21 @@
 import { Deck } from '../models';
 import { Request, Response } from 'express';
 
+type deckByIdParams = {
+	id: string;
+};
+
+type cardsType = {
+	title: string;
+	prompt: string;
+	solution: string;
+};
+
+type deckType = {
+	deckName: string;
+	cards: cardsType[];
+};
+
 export const getAllDecks = async (req: Request, res: Response) => {
 	try {
 		const decks = await Deck.find();
@@ -19,8 +34,9 @@ export const getAllDecks = async (req: Request, res: Response) => {
 };
 
 export const getDeckById = async (req: Request, res: Response) => {
+	const { id }: deckByIdParams = req.params as deckByIdParams;
 	try {
-		const deck = await Deck.findOne({ _id: req.params.id });
+		const deck = await Deck.findOne({ _id: id });
 
 		if (!deck) {
 			res.status(404).json({ message: 'No deck found with that ID' });
@@ -36,8 +52,9 @@ export const getDeckById = async (req: Request, res: Response) => {
 };
 
 export const createDeck = async (req: Request, res: Response) => {
+	const newDeck: deckType = req.body as deckType;
 	try {
-		const deck = await Deck.create(req.body);
+		const deck = await Deck.create(newDeck);
 
 		if (!deck) {
 			res.status(404).json({ message: 'Could not create deck' });
@@ -66,8 +83,9 @@ export const updateDeck = async (req: Request, res: Response) => {
 };
 
 export const deleteDeck = async (req: Request, res: Response) => {
+	const { id }: deckByIdParams = req.body as deckByIdParams;
 	try {
-		const deck = await Deck.findByIdAndRemove({ _id: req.body.deckId });
+		const deck = await Deck.findByIdAndRemove({ _id: id });
 
 		if (!deck) {
 			res.status(404).json({ message: 'No deck found with that ID' });
