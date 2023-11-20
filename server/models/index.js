d15f8a0bebe3fd01099ff211371c2f203cbb1af7
Brahm Van Houzen <brahmvanh@gmail.com>
2023-11-19 20:48:26 -0500

    removed the rest of the uneeded ts code and installed ts-node

--- server/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Deck = void 0;
-const Deck_1 = require("./Deck");
-Object.defineProperty(exports, "Deck", { enumerable: true, get: function () { return Deck_1.Deck; } });
